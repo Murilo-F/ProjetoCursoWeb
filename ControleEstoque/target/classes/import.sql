d3f42ab4a1c954d48122db3defd3baf0b53707e5
jmueloschat <jmueloschat@gmail.com>
2023-05-28 13:45:05 -0300

    backend

--- ControleEstoque/target/classes/import.sql
+++ ControleEstoque/target/classes/import.sql
@@ -1,3 +1,4 @@
+@ -1,9 +0,0 @@
 insert into unidades_Medida (ds_Unimed, ds_Sigla) values('Quilos', 'KG');
 insert into unidades_Medida (ds_Unimed, ds_Sigla) values('Metros', 'M');
 insert into unidades_Medida (ds_Unimed, ds_Sigla) values('Unidades', 'UN');
